bcbe0e2029e2fbdc100b21ab415f7632d821e651
laurentiuspurba <lpurba@gannett.com>
2019-09-12 13:17:09 -0400

    Initial commit

---
+++ TestJob.groovy
@@ -0,0 +1,48 @@
+/*** BEGIN META {
+  "name" : "A seed job",
+  "comment" : "A seed job that will create a job for testing purpose"
+  "parameters": []
+  "authors": [
+     { name: "Laurentius Purba"}
+  ]
+ */
+
+String basePath = 'dsljobs'
+
+folder(basePath) {
+   description 'DSL generated folder'
+}
+
+pipelineJob("$basePath/pipeline-seed-job") {
+  description()
+  // daysToKepp, numToKepp, artifactDaysToKeep, artifactNumToKeep
+  logRotator(2, -1, 1, -1)
+
+  definition {
+    cps {
+      sandbox(true)
+      script('''
+        pipeline {
+          agent any
+          stages {
+            stage('Running simple job') {
+              steps {
+                echo 'Hello world'
+              }
+            }
+            stage('Showing jenkins uid') {
+              steps {
+                sh 'id'
+              }
+            }
+            stage('Process statistics') {
+              steps {
+                ps -a
+              }
+            }
+          }
+        }
+      '''.stripIndent().trim())
+    }
+  }
+}
